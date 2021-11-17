35e1edd932dbaf9f929bbe6e24fac9f6fee55907
coding-my-world <1950274892@qq.com>
2021-11-17 18:44:08 +0800

    Merge remote-tracking branch 'origin/master'

--- python大作业/学习代码/demo5.py
+++ python大作业/学习代码/demo5.py
@@ -2,8 +2,9 @@
 # 时间:2021/11/17 18:07
 # 功能:将字典写入excel表格中
 from openpyxl import Workbook
-from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font
+from openpyxl.utils import get_column_letter
+
 data = [
     {
         'name': '小白',
@@ -40,11 +41,13 @@ ws.append(title)
 for person in data:
     ws.append(list(person.values()))
 
+# 计算平均值
 for col in range(2, 5):  # range是左闭右开
     char = get_column_letter(col)
     ws[char + '6'] = f"=AVERAGE({char + '2'}:{char + '5'})"
 
-for col in range(1,5):
+# 修改字体颜色
+for col in range(1, 5):
     char = get_column_letter(col)
-    ws[char+'1'].font=Font(bold=True,color='000000FF')
+    ws[char + '1'].font = Font(bold=True, color='000000FF')
 wb.save('data.xlsx')
